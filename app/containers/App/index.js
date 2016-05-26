13926ee44ed6fbb38101cd43c2caa68a254933cb
havard024 <havard@zeppelin.no>
2016-05-26 11:48:30 +0200

    feat(app): Added react-toolbox and react-toolbox layout example

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,31 +7,72 @@
  */
 
 import React from 'react';
+import { AppBar, Checkbox, IconButton, Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
 
 // Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
 import 'sanitize.css/sanitize.css';
 
-import Img from 'components/Img';
-import Footer from 'components/Footer';
-import Banner from './banner-metal.jpg';
-import A from 'components/A';
-
 import styles from './styles.css';
 
-function App(props) {
-  return (
-    <div className={styles.wrapper}>
-      <A className={styles.logoWrapper} href="https://twitter.com/mxstbr">
-        <Img className={styles.logo} src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      {props.children}
-      <Footer />
-    </div>
-  );
+/* eslint-disable react/prefer-stateless-function */
+export default class App extends React.Component {
+
+  static PropTypes = {
+    chidlren: React.PropTypes.node,
+  };
+
+  state = {
+    drawerActive: false,
+    drawerPinned: false,
+    sidebarPinned: false,
+    inverse: true,
+  };
+
+  toggleDrawerActive = () => {
+    this.setState({ drawerActive: !this.state.drawerActive });
+  };
+
+  toggleDrawerPinned = () => {
+    this.setState({ drawerPinned: !this.state.drawerPinned });
+  };
+
+  toggleSidebar = () => {
+    this.setState({ sidebarPinned: !this.state.sidebarPinned });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <NavDrawer>
+          active={this.state.drawerActive}
+          pinned={this.state.drawerPinned} permanentAt="xxxl"
+          onOverlayClick={this.toggleDrawerActive}
+         >
+          <p>
+            Navigation, account switcher, etc. go here.
+          </p>
+        </NavDrawer>
+        <Panel>
+          <AppBar><IconButton inverse={this.state.inverse} icon="menu" onClick={this.toggleDrawerActive} /></AppBar>
+          <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
+            <h1>Curato - Statistikkmodul</h1>
+            <p>Main content goes here.</p>
+            <Checkbox label="Pin drawer" checked={this.state.drawerPinned} onChange={this.toggleDrawerPinned} />
+            <Checkbox label="Show sidebar" checked={this.state.sidebarPinned} onChange={this.toggleSidebar} />
+            {this.props.children}
+          </div>
+        </Panel>
+        <Sidebar pinned={this.state.sidebarPinned} width={5}>
+          <div styles={styles.wrapper}><IconButton icon="close" onClick={this.toggleSidebar} /></div>
+          <div style={{ flex: 1 }}>
+            <p>Supplement content goes here.</p>
+          </div>
+        </Sidebar>
+      </Layout>
+    );
+  }
 }
 
 App.propTypes = {
   children: React.PropTypes.node,
 };
-
-export default App;
